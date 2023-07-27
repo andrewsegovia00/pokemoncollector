b9438afb0c5616b8a6cfdb53301f5e0838c27e26
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-07-27 08:10:19 -0700

    Implemented Create, Delete, Update

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    types = models.CharField(max_length=100)
+    rarity = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.id})"
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"cat_id": self.id})
+
+
+# pokemon = [
+#   {'name': 'Venusaur-EX', 'types': 'Grass', 'rarity': 'Rare Holo EX', 'artist': 'Eske Yoshinob', 'img': 'https://images.pokemontcg.io/xy1/1_hires.png'},
+#   {'name': 'Xerneas-EX', 'types': 'Fairy', 'rarity': 'Rare Ultra', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy1/146_hires.png'},
+#   {'name': 'Charizard-EX', 'types': 'Fire', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/100_hires.png'},
+#   {'name': 'M Lucario-EX', 'types': 'Fighting', 'rarity': 'Rare Holo EX', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy3/55_hires.png'},
+#   {'name': 'Magnezone-EX', 'types': 'Lightning', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/101_hires.png'},
+#   {'name': 'Toxicroak-EX', 'types': 'Psychic', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/102_hires.png'},
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,50 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Pokemon
 
-pokemon = [
-  {'name': 'Venusaur-EX', 'types': 'Grass', 'rarity': 'Rare Holo EX', 'artist': 'Eske Yoshinob', 'img': 'https://images.pokemontcg.io/xy1/1_hires.png'},
-  {'name': 'Xerneas-EX', 'types': 'Fairy', 'rarity': 'Rare Ultra', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy1/146_hires.png'},
-  {'name': 'Charizard-EX', 'types': 'Fire', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/100_hires.png'},
-  {'name': 'M Lucario-EX', 'types': 'Fighting', 'rarity': 'Rare Holo EX', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy3/55_hires.png'},
-  {'name': 'Magnezone-EX', 'types': 'Lightning', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/101_hires.png'},
-  {'name': 'Toxicroak-EX', 'types': 'Psychic', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/102_hires.png'},
-]
+# pokemon = [
+#   {'name': 'Venusaur-EX', 'types': 'Grass', 'rarity': 'Rare Holo EX', 'artist': 'Eske Yoshinob', 'img': 'https://images.pokemontcg.io/xy1/1_hires.png'},
+#   {'name': 'Xerneas-EX', 'types': 'Fairy', 'rarity': 'Rare Ultra', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy1/146_hires.png'},
+#   {'name': 'Charizard-EX', 'types': 'Fire', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/100_hires.png'},
+#   {'name': 'M Lucario-EX', 'types': 'Fighting', 'rarity': 'Rare Holo EX', 'artist': '5ban Graphics', 'img': 'https://images.pokemontcg.io/xy3/55_hires.png'},
+#   {'name': 'Magnezone-EX', 'types': 'Lightning', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/101_hires.png'},
+#   {'name': 'Toxicroak-EX', 'types': 'Psychic', 'rarity': 'Rare Ultra', 'artist': 'Ryo Ueda', 'img': 'https://images.pokemontcg.io/xy2/102_hires.png'},
+# ]
+
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html', {'pokemon': pokemon})
+    pokemon = Pokemon.objects.all()
+    return render(request, "home.html", {"pokemon": pokemon})
+
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def pokemon_index(request):
-  return render(request, 'pokemon/index.html', {
-    'pokemon': pokemon
-  })
+    pokemon = Pokemon.objects.all()
+    return render(request, "pokemon/index.html", {"pokemon": pokemon})
+
+
+def pokemon_detail(request, cat_id):
+    pokemon = Pokemon.objects.get(id=cat_id)
+    return render(request, "pokemon/detail.html", {"pokemon": pokemon})
+
+
+class PokemonCreate(CreateView):
+    model = Pokemon
+    fields = "__all__"
+    # success_url = "/pokemon/"
+
+
+class PokemonUpdate(UpdateView):
+    model = Pokemon
+    fields = "__all__"
+    # success_url = "/pokemon/"
+
+
+class PokemonDelete(DeleteView):
+    model = Pokemon
+    fields = "__all__"
+    success_url = "/pokemon/"
